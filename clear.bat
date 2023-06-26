29fefc53abbcfd1781dfd1885c6e49ccdbfd74ea
androllen <androllen@hotmail.com>
2023-06-27 00:02:45 +0800

    clear node_modules

---
+++ clear.bat
@@ -0,0 +1,31 @@
+@echo off
+
+cd /D "%cd%\node_modules"
+echo %cd%
+
+set   CurTime=%time%
+set   mm=%CurTime:~3,2%
+set   ss=%CurTime:~6,2%
+set   beforeDateTime=%mm%:%ss%
+
+
+for /d %%i in (*.*) do (
+  echo %%i
+  rd /s /q "%%i"
+)
+
+
+for  %%i in (*.*) do (
+  del /s /q "%%i"
+)
+
+set   CurTime=%time%
+set   mm=%CurTime:~3,2%
+set   ss=%CurTime:~6,2%
+set   afterDateTime=%mm%:%ss%
+set   beforeDateTime
+set   afterDateTime
+
+cd ..
+echo Clear OVER!
+echo. & pause 
