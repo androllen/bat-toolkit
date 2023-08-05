457d95015f959bf8f898703e6dae833c9667aa45
androllen <androllen@hotmail.com>
2023-08-05 23:14:56 +0800

    update

--- TomatoUniversalTimer.bat
+++ TomatoUniversalTimer.bat
@@ -30,6 +30,23 @@ if "%input%" neq "" (
 	set input_="game is over"
 )
 
+rem Get the time from WMI - at least that's a format we can work with
+set X=
+for /f "skip=1 delims=" %%x in ('wmic os get localdatetime') do if not defined X set X=%%x
+echo.%X%
+
+rem dissect into parts
+set DATE.YEAR=%X:~0,4%
+set DATE.MONTH=%X:~4,2%
+set DATE.DAY=%X:~6,2%
+set DATE.HOUR=%X:~8,2%
+set DATE.MINUTE=%X:~10,2%
+set DATE.SECOND=%X:~12,2%
+set DATE.FRACTIONS=%X:~15,6%
+set DATE.OFFSET=%X:~21,4%
+
+echo %DATE.YEAR%-%DATE.MONTH%-%DATE.DAY% %DATE.HOUR%:%DATE.MINUTE%:%DATE.SECOND%
+
 echo input_h=%input_h%
 echo input_m=%input_m%
 echo input_s=%input_s%
@@ -46,6 +63,12 @@ set /a end_h=1%init_h%-100+%input_h%
 set /a end_m=1%init_m%-100+%input_m%
 set /a end_s=1%init_s%-100+%input_s%
 echo %end_h%:%end_m%:%end_s%
+
+set current_year=%DATE.YEAR%
+set current_month=%DATE.MONTH:~1,1%
+set current_day=%DATE.DAY:~1,1%
+echo %current_year%_%current_month%_%current_day%
+
 
 if %end_s% GEQ 60 (
 	set /a next_m=%end_s%/60
@@ -68,29 +91,22 @@ if %end_s% GEQ 60 (
 	echo abcd=!end_h!:!end_m!:!end_s!
 )
 
-set current_year=%date:~0,4%
-set current_month=%date:~5,2%
-set current_day=%date:~8,2%
-echo %current_year%_%current_month%_%current_day%
-
-
-
 
 rem 跨天
 if %end_h% GEQ 24 (
 	rem 计算跨过多少天
-	set /a next_day=%end_h%/24
-	echo next_day=!next_day!
+	set /a need_day=%end_h%/24
+	echo need_day=!need_day!
 	rem 计算当月取余天数
 	set /a end_h=%end_h%%%24
-	echo end_h=!end_h!
-	set /a current_month=1%date:~5,2%-100
-	echo current_month=%current_month%
+	echo end_datetime=!end_h!:!end_m!:!end_s!
+	set current_month=%current_month%
+	echo current_month_=!current_month!
 
 	call:monthcall
-	echo current_month1=%current_month% month has !current_month_day!
-	set /a current_has_day=!current_month_day!-%current_day%
-	echo this_month_remain_day=!current_has_day!
+	echo !current_month! month has !current_month_day!
+	set /a current_month_remain_day=!current_month_day!-%current_day%
+	echo current_month_remain_day=!current_month_remain_day!
 
 	goto listloop
 ) else (
@@ -107,31 +123,40 @@ if %end_h% GEQ 24 (
 EXIT /B %ERRORLEVEL% 
 
 :listloop
-set /a current_month=%current_month%+1
-call:monthcall
-echo current_month=%current_month% month has !current_month_day!
-set /a current_has_day=!current_has_day!+!current_month_day!
-echo this_month_plus_day=!current_has_day!
 
-if !current_has_day! GEQ !next_day! (
+rem 没有跨月	
+if !current_month_remain_day! GEQ !need_day! (
 	goto endcall
 
 ) else (
-	if !current_month! equ 12 (
-		set current_month=0
-		set /a current_year=%current_year%+1
-		echo current_month=!current_month!
-		
+	rem 有跨月	
+	set /a current_month=%current_month%+1
+	call:monthcall
+	echo %current_month% month has_ !current_month_day!
+	set /a current_month_remain_day=!current_month_remain_day!+!current_month_day!
+	echo current_month_remain_day=!current_month_remain_day!
+	if !current_month_remain_day! GEQ !need_day! (
+		goto endcall
+
+	) else (
+		if !current_month! equ 12 (
+			set current_month=0
+			set /a current_year=%current_year%+1
+			echo current_month=!current_month!
+			
+		)
+		call:listloop
 	)
-	call:listloop
 )
+
+
 goto:eof
 
 
 :endcall
-set /a remain_day=!current_has_day!-!next_day!
+set /a remain_day=!current_month_remain_day!-!need_day!
 set /a current_day=!current_month_day!-!remain_day!
-echo over=%current_year%_%current_month%_!current_day!
+echo Over_Datetime=%current_year%_%current_month%_!current_day!
 goto start
 
 goto:eof
@@ -145,13 +170,13 @@ set /a current_t_h=1%current_time:~0,2%-100
 set /a current_t_m=1%current_time:~3,2%-100
 set /a current_t_s=1%current_time:~6,2%-100
 
-set current_d_year=%date:~0,4%
-set /a current_d_month=1%date:~5,2%-100
-set /a current_d_day=1%date:~8,2%-100
+set current_d_year=%DATE.YEAR%
+set /a current_d_month=1%DATE.MONTH%-100
+set /a current_d_day=1%DATE.DAY%-100
 
 
-echo current_year_month_day_hour_min_sec:%current_d_year%-%current_d_month%-%current_d_day% %current_t_h%:%current_t_m%:%current_t_s%
-echo next_year_month_day_hour_min_sec:%current_year%_%current_month%_!current_day! %end_h%:%end_m%:%end_s%
+echo Current_Datatime:%current_d_year%_%current_d_month%_%current_d_day% %current_t_h%:%current_t_m%:%current_t_s%
+echo Expiry_Datetime:%current_year%_%current_month%_!current_day! %end_h%:%end_m%:%end_s%
 echo.
 
 if %current_d_year% GEQ !current_year! (
